572797ff41359453643ae9893ca82c72fafcc447
dimapb <1st.trillionaire.dm@gmail.com>
2024-12-04 15:22:45 +0300

    Bot

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ from datetime import datetime
 from time import sleep
 
 from src.algorithm import Algorithm
-from src.config import BASE_COIN, QUOTE_COIN, INTERVAL
+from config import BASE_COIN, QUOTE_COIN, INTERVAL
 from src.functools import modified_
 from src.provider import ByBit
 
 
 class Bot:
-    def __init__(self, base_coin='BTC', quote_coin='ETH', interval='60'):
+    def __init__(self, base_coin='BTC', quote_coin='ETH', interval=60):
         self.base_coin = base_coin.upper() + 'USDT'
         self.quote_coin = quote_coin.upper() + 'USDT'
-        self.interval = interval
+        self.interval = interval * 60
         self.algorithm = Algorithm()
         self.base_coin = ByBit(
             symbol=self.base_coin,
@@ -24,14 +24,12 @@ class Bot:
         )
 
     def work(self):
-        """ Бесконечный цикл для продолжения работы в реальном времени """
-        interval = int(self.interval)
-        self.trigger_first_iteration(interval)
-        self.restarter(interval*60, self.iterate)
+        """ Продолжение работы в реальном времени """
+        self.restarter(self.interval, self.iterate)
 
     def restarter(self, interval, func):
         """
-        Постоянный перезапуск функции через интервал
+        Перезапуск каждой итерации через интервал
         :param interval: Время в секундах
         :param func: Функция, которую надо перезапускать
         """
@@ -39,29 +37,21 @@ class Bot:
         func()
 
     def iterate(self):
-        """ Одна совершаемая ботом итерация """
+        """ Одна, совершаемая ботом итерация """
         base_kline = self.base_coin.get_kline()[-1]
         quote_kline = self.quote_coin.get_kline()[-1]
 
         own_price_change = self.algorithm.compare_klines(modified_(base_kline), modified_(quote_kline))
-        print(f"{own_price_change}%")
+        print(f"{own_price_change}%", datetime.now())
 
-    @staticmethod
-    def trigger_first_iteration(interval):
-        """ Запуск первой итерации по времени в зависимости от интервала """
-        if interval < 60:
-            while True:
-                if int(str(datetime.now())[14:16]) % interval == 0 and str(datetime.now())[17:19] == '00':
-                    sleep(2)
-                    return
-                sleep(1)
-
-        elif interval <= 1440:
-            while True:
-                if int(str(datetime.now())[11:13]) % interval / 60 == 0 and str(datetime.now())[14:16] == '00':
-                    sleep(2)
-                    return
-                sleep(60)
+    def start_work(self):
+        """ Задержка перед началом работы позволяющая получать клайны через 3 секунды после их завершения """
+        while True:
+            now = datetime.now()
+            if (int(now.hour) * 60 * 60 + int(now.minute) * 60 + int(now.second)) % self.interval == 0:
+                sleep(2)
+                return self.work()
+            sleep(1)
 
 
 if __name__ == '__main__':
@@ -69,4 +59,4 @@ if __name__ == '__main__':
         base_coin=BASE_COIN,
         quote_coin=QUOTE_COIN,
         interval=INTERVAL,
-    ).work()
+    ).start_work()
